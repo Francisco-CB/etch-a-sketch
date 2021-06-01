4b8ec701455c38959b1b36031a3837088d580403
Francisco Cubillos B <francisco.cubillos@ug.uchile.cl>
2021-05-31 22:40:20 -0400

    Se mejora distribución de celdas de grilla

--- sketch-code.js
+++ sketch-code.js
@@ -7,15 +7,14 @@ function removeAllChildNodes(parent) {
 function selectGridSize(){
     rango = Math.floor(window.prompt("Enter grid size: "));
 
-    if(rango > 64 || rango == 0 || typeof(rango) !== "number"){
+    if(rango > 70 || rango == 0 || typeof(rango) !== "number"){
         rango = 1; // EN CASO DE QUE PONGA UN VALOR ILEGAL
     }
     
-    cellSize = container.getBoundingClientRect()['width']/rango;
     removeAllChildNodes(container);
     
-    container.style.setProperty('grid-template-columns', `repeat(${rango}, ${cellSize}px)`);
-    container.style.setProperty('grid-template-rows', `repeat(${rango}, ${cellSize}px)`);
+    container.style.setProperty('grid-template-columns', `repeat(${rango}, 1fr)`);
+    container.style.setProperty('grid-template-rows', `repeat(${rango}, 1fr)`);
 
     // SE AGREGAN LAS CELDAS
     for(let i=0; i<rango; i++){
@@ -30,7 +29,6 @@ function selectGridSize(){
 let div; // VARIABLE PARA GUARDAR DIVS A AGREGAR A DIV PRINCIPAL
 let rango; // RANGO DE GRID
 let children; // PARA GUARDAR CHILDREN DE DIV PRINCIPAL
-let cellSize; // TAMAÑO QUE TENDRAN LAS CELDAS
 const container = document.getElementById("div-principal");
 const sizeButton = document.getElementById("size-button");
 
